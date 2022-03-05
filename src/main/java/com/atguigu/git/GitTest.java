675eee853d3b6b79b0f59007a29aee0876616e5b
bugbug222 <49337762+bugbug222@users.noreply.github.com>
2022-03-05 16:26:11 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master test ");
         System.out.println("hot-fix commit2");
         System.out.println("push again");
+        System.out.println("pull test");
     }
 }
