ec53d25cc560467c5c21bfd91729e5526bc121be
chensidi <1455142609@qq.com>
2022-03-05 14:28:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author chensidi
+ * @create 2022-03-05 14:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
